74646a7ff0092aa79b2c6aa63458e0d5e9bae371
Jack Janes <11janesj@gmail.com>
2014-04-24 12:06:11 -0400

    refined gui

--- src/edu/wakeforest/drone/gui/GUI.java
+++ src/edu/wakeforest/drone/gui/GUI.java
@@ -1,4 +1,4 @@
-package de.yadrone.apps.master;
+package edu.wakeforest.drone.gui;
 
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -9,16 +9,15 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-import keyboard.listener.KeyboarFrame;
 import de.yadrone.apps.tutorial.TutorialVideoListener;
 import de.yadrone.base.ARDrone;
 import de.yadrone.base.IARDrone;
 import de.yadrone.base.command.CommandManager;
 import de.yadrone.base.video.VideoImage;
-import de.yadrone.apps.controlcenter.plugins.keyboard.KeyboardCommandManager;
+import edu.wakeforest.drone.keyboardframe.KeyboardFrame;
 
 
-public class Gui extends JFrame{
+public class GUI extends JFrame{
 	private JLabel label;
 	private JButton buttonTakeoff;
 	private JButton buttonLand;
@@ -26,11 +25,13 @@ public class Gui extends JFrame{
 	private JButton buttonRight;
 	private JButton buttonForward;
 	private JButton buttonBackwards;
+	private JButton buttonBegin;
 	private IARDrone drone;
 	private BufferedImage img;
 	
 	//constructor
-		public Gui(ARDrone drone){
+		public GUI(ARDrone dronea){
+			drone = dronea;
 			CommandManager cmd = drone.getCommandManager();
 			new TutorialVideoListener(drone);
 			new VideoImage();
@@ -39,8 +40,6 @@ public class Gui extends JFrame{
 			// build the GUI for the user
 			setLayout(new FlowLayout());
 			
-			label = new JLabel("Waiting for user input");
-			add(label);
 			
 			buttonTakeoff = new JButton("Takeoff");
 			add(buttonTakeoff);
@@ -48,6 +47,13 @@ public class Gui extends JFrame{
 			buttonLand = new JButton("Land");
 			add(buttonLand);
 			
+			buttonBegin = new JButton("Begin Search");
+			add (buttonBegin);
+			
+			label = new JLabel("Waiting for user input");
+			add(label);
+			
+			/*
 			buttonRight = new JButton("Right");
 			add(buttonRight);
 			
@@ -59,27 +65,35 @@ public class Gui extends JFrame{
 			
 			buttonBackwards = new JButton("Backward");
 			add(buttonBackwards);
+			*/
 			
-			event1 t = new event1();
+			
+			takeoffe t = new takeoffe();
 			buttonTakeoff.addActionListener(t);
 			
-			event2 l = new event2();
+			lande l = new lande();
 			buttonLand.addActionListener(l);
 			
-			event3 right = new event3();
-			buttonLand.addActionListener(right);
+			/*
+			righte right = new righte();
+			buttonRight.addActionListener(right);
 			
-			event4 left = new event4();
-			buttonLand.addActionListener(left);
+			lefte left = new lefte();
+			buttonLeft.addActionListener(left);
 			
-			event5 forward = new event5();
-			buttonLand.addActionListener(forward);
+			forwarde forward = new forwarde();
+			buttonForward.addActionListener(forward);
 			
-			event6 backwards = new event6();
-			buttonLand.addActionListener(backwards);
+			backwardse backwards = new backwardse();
+			buttonBackwards.addActionListener(backwards);
+			*/
+			
+			begine begin = new begine();
+			buttonBegin.addActionListener(begin);
+			
 		}
 		
-		public class event1 implements ActionListener{
+		public class takeoffe implements ActionListener{
 			public void actionPerformed(ActionEvent t){
 				label.setText("Taking off");
 				drone.takeOff();
@@ -87,47 +101,55 @@ public class Gui extends JFrame{
 			}
 		}
 		
-		public class event2 implements ActionListener{
+		public class lande implements ActionListener{
 			public void actionPerformed(ActionEvent l){
 				label.setText("Landing");
 				drone.landing();
 			}
 		}
 		
-		public class event3 implements ActionListener{
+		/*
+		public class righte implements ActionListener{
 			public void actionPerformed(ActionEvent right){
 				label.setText("Moving right");
-				// move drone right
+				drone.goRight();
 			}
 		}
 		
-		public class event4 implements ActionListener{
+		public class lefte implements ActionListener{
 			public void actionPerformed(ActionEvent left){
 				label.setText("Moving left");
-				// move drone left
+				drone.goLeft();
 			}
 		}
 		
-		public class event5 implements ActionListener{
+		public class forwarde implements ActionListener{
 			public void actionPerformed(ActionEvent forward){
 				label.setText("Moving forward");
-				// move drone forward
+				drone.forward();
 			}
 		}
 		
-		public class event6 implements ActionListener{
+		public class backwardse implements ActionListener{
 			public void actionPerformed(ActionEvent backwards){
 				label.setText("Moving backwards");
 				// move drone back
 			}
 		}
+		*/
 		
-		/*public static void main(String args[]){
-			Gui gui = new Gui();
+		public class begine implements ActionListener{
+			public void actionPerformed(ActionEvent begin){
+				label.setText("Beginning search");
+				// call image processing function
+			}
+		}
+		
+		public static void create(GUI gui){
 			gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			gui.setSize(200,200);
+			gui.setSize(170,150);
 			gui.setVisible(true);
 			gui.setTitle("Controller");
-		}*/
+		}
 		
 	}
